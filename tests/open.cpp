27f7bf22f9ef1592d69773b03e096bf5446b1ce5
Zbigniew Romanowski <romz@wp.pl>
2018-02-26 19:49:08 +0100

    Test: Open database for various block sizes

--- tests/open.cpp
+++ tests/open.cpp
@@ -1,32 +1,56 @@
 #include <gtest/gtest.h>
 #include <gdbm.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 TEST(gdbm, open)
 {
     GDBM_FILE dbf;
     char dbname[] = "./tmp.db";
+    const int block_size = 0;
+    const int mode = 0600;
 
-    dbf = gdbm_open( dbname, 0, GDBM_NEWDB, 0600, NULL );
+    dbf = gdbm_open( dbname, block_size, GDBM_NEWDB, mode, NULL );
     EXPECT_TRUE( dbf );
     EXPECT_NO_THROW( gdbm_close( dbf ) );
 
-    dbf = gdbm_open( dbname, 0, GDBM_READER, 0600, NULL );
+    dbf = gdbm_open( dbname, block_size, GDBM_READER, mode, NULL );
     EXPECT_TRUE( dbf );
     EXPECT_NO_THROW( gdbm_close( dbf ) );
 
-    dbf = gdbm_open( dbname, 0, GDBM_WRITER, 0600, NULL );
+    dbf = gdbm_open( dbname, block_size, GDBM_WRITER, mode, NULL );
     EXPECT_TRUE( dbf );
     EXPECT_NO_THROW( gdbm_close( dbf ) );
 
     EXPECT_TRUE( unlink( dbname ) == 0 );
 
 
-    dbf = gdbm_open( dbname, 0, GDBM_READER, 0600, NULL );
+    dbf = gdbm_open( dbname, block_size, GDBM_READER, mode, NULL );
     EXPECT_FALSE( dbf );
 
-    dbf = gdbm_open( dbname, 0, GDBM_WRITER, 0600, NULL );
+    dbf = gdbm_open( dbname, block_size, GDBM_WRITER, mode, NULL );
     EXPECT_FALSE( dbf );
 
+}
+
+
+TEST(gdbm, open_block_size)
+{
+    GDBM_FILE dbf;
+    char dbname[] = "./tmp.db";
+    int block_size = 0;
+    const int mode = 0600;
+
+    srand( 1234 );
+
+    for( int i = 1; i < 100; i++)
+    {
+        block_size = rand() % ( 1 << 16 );
+        // printf( "%d\n", block_size );
+        dbf = gdbm_open( dbname, block_size, GDBM_NEWDB, mode, NULL );
+        EXPECT_TRUE( dbf );
+        EXPECT_NO_THROW( gdbm_close( dbf ) );
+    }
+    EXPECT_TRUE( unlink( dbname ) == 0 );
 }
 
