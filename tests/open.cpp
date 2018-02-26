5c4624f09d875a740883778c83f5534a35aa1bf0
Zbigniew Romanowski <romz@wp.pl>
2018-02-26 19:09:21 +0100

    Add framework for tests

---
+++ tests/open.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+TEST(Db, Open)
+{
+/*    const std::string path = UnixFile::GetTempPath();
+    std::size_t frameNo = 13;
+
+    Db db( path, frameNo );
+    EXPECT_NO_THROW( db.CreteHeapFile() );
+*/
+}
